f469692663db35b7f2cfa349b7ae385f217d1574
Jiseong Kim <jiseong330@gmail.com>
2022-08-20 11:48:38 +0900

    second commit

--- app.js
+++ app.js
@@ -12,69 +12,12 @@ const $backButton = document.querySelector('#backButton');
 const $result = document.querySelector('.result');
 const participants = [];
 const prizes = [];
+const participantsName = ['영수', '아인', '민지', '지은', '희원', '국진', '유경'];
+const prizesName = ['딸기', '사과', '바나나', '블루베리', '파인애플', '체리', '망고'];
 const rows = [];
 let participantsNum;
 let row = 5;
 let col;
-
-function legFunc() {
-  const legTds = document.querySelectorAll('td.showLeg');
-  legTds.forEach((td) => {
-    const cellIndex = td.cellIndex;
-    const inputIndex = (cellIndex - 1) / 2;
-    prizes.splice(inputIndex, 2, prizes[inputIndex + 1], prizes[inputIndex]);
-    // console.log(prizes);
-  });
-}
-
-function cleanLegs() {
-  rows.forEach((row) => {
-    row.forEach((td) => {
-      let cellIndex = td.cellIndex;
-      if (cellIndex % 2 === 1) {
-        td.classList.remove('showLeg');
-        td.classList.add('empty');
-      }
-    })
-  })
-}
-
-function drawLegs() {
-  let percent = 0.7;
-  rows.forEach((row) => {
-    row.forEach((td) => {
-      let cellIndex = td.cellIndex;
-      if (cellIndex % 2 === 1) {
-        if (Math.random() < percent) {
-          td.classList.remove('empty');
-          td.classList.add('showLeg');
-        }
-        if (cellIndex >= 3 && row[cellIndex - 2].className === 'showLeg') {
-          td.classList.remove('showLeg');
-          td.classList.add('empty');  
-        }
-      }
-    })
-  })
-}
-
-function drawTable() {
-  const $table = document.createElement('table');
-  col = participantsNum * 2 - 1;
-  for (let i = 0; i < row; i++) {
-    const cells = [];
-    const $tr = document.createElement('tr');
-    for (let j = 0; j < col; j++) {
-      const $td = document.createElement('td');
-      $tr.append($td);
-      cells.push($td);
-    }
-    $table.append($tr);
-    rows.push(cells);
-  }
-  $legsWrapper.append($table);
-  cleanLegs();
-}
 
 function onSubmit(event) {
   event.preventDefault();
@@ -88,8 +31,11 @@ function onSubmit(event) {
   participantsNum = parseInt(event.target.participantsNum.value); //event.target[''].value
   for (let i = 0; i < participantsNum; i++) {
     const participant = document.createElement('input');
+    participant.value = participantsName[i];
+    // console.log(participantsName[i]);
     participant.id = 'participant' + i;
     const prize = document.createElement('input');
+    prize.value = prizesName[i];
     prize.id = 'prize' + i;
     $participantsWrapper.append(participant);
     $prizesWrapper.append(prize);
@@ -119,6 +65,53 @@ function start(event) {
 }
 $getNamesForm.addEventListener('submit', start);
 
+function drawTable() {
+  const $table = document.createElement('table');
+  col = participantsNum * 2 - 1;
+  for (let i = 0; i < row; i++) {
+    const cells = [];
+    const $tr = document.createElement('tr');
+    for (let j = 0; j < col; j++) {
+      const $td = document.createElement('td');
+      $tr.append($td);
+      cells.push($td);
+    }
+    $table.append($tr);
+    rows.push(cells);
+  }
+  $legsWrapper.append($table);
+  cleanLegs();
+}
+
+function drawLegs() {
+  let percent = 0.7;
+  rows.forEach((row) => {
+    row.forEach((td) => {
+      let cellIndex = td.cellIndex;
+      if (cellIndex % 2 === 1) {
+        if (Math.random() < percent) {
+          td.classList.remove('empty');
+          td.classList.add('showLeg');
+        }
+        if (cellIndex >= 3 && row[cellIndex - 2].className === 'showLeg') {
+          td.classList.remove('showLeg');
+          td.classList.add('empty');  
+        }
+      }
+    })
+  })
+}
+
+function legFunc() {
+  const legTds = document.querySelectorAll('td.showLeg');
+  legTds.forEach((td) => {
+    const cellIndex = td.cellIndex;
+    const inputIndex = (cellIndex - 1) / 2;
+    prizes.splice(inputIndex, 2, prizes[inputIndex + 1], prizes[inputIndex]);
+    // console.log(prizes);
+  });
+}
+
 function showResult() {
   for (let i = 0; i < participantsNum; i++) {
     $result.append(
@@ -134,12 +127,12 @@ $resultButton.addEventListener('click', showResult);
 function goBack() {
   const parInputs = $participantsWrapper.querySelectorAll('input');
   const priInputs = $prizesWrapper.querySelectorAll('input');
-  parInputs.forEach((e) => {
-    e.value = '';
-  });
-  priInputs.forEach((e) => {
-    e.value = '';
-  });
+  // parInputs.forEach((e) => {
+  //   e.value = '';
+  // });
+  // priInputs.forEach((e) => {
+  //   e.value = '';
+  // });
   $participantsNum.value = '';
   $result.textContent = '';
   $backButton.style.display = 'none';
@@ -147,3 +140,15 @@ function goBack() {
   cleanLegs();
 }
 $backButton.addEventListener('click', goBack);
+
+function cleanLegs() {
+  rows.forEach((row) => {
+    row.forEach((td) => {
+      let cellIndex = td.cellIndex;
+      if (cellIndex % 2 === 1) {
+        td.classList.remove('showLeg');
+        td.classList.add('empty');
+      }
+    })
+  })
+}
